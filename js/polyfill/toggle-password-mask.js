2f821864d394383fe621eb7ce038bc004ed3d74c
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2013-11-02 10:05:00 -0700

    refactored checkbox and radio styles toggle password mask utility

--- js/polyfill/toggle-password-mask.js
+++ js/polyfill/toggle-password-mask.js
@@ -1,28 +1,16 @@
 YUI.add('toggle-password-mask', function(Y) {
-  var showPassword = Y.one('#show-password'),
-    password = Y.one('#password'),
-    showPasswordIcon = Y.one('#show-password-icon'),
-    showPasswordLabel = Y.one('#show-password-label'),
+  var password = Y.one('#password'),
     togglePasswordMask;
 
   togglePasswordMask = function(e) {
-    e.halt();
-    if(password.get('type') === 'password') {
+    if(e.target.get('checked')) {
       password.set('type', 'text');
-      showPasswordIcon.addClass('checked');
-      showPasswordIcon.removeClass('unchecked');
-      showPasswordLabel.setAttribute('aria-checked', 'true');
     } else {
       password.set('type', 'password');
-      showPasswordIcon.addClass('unchecked');
-      showPasswordIcon.removeClass('checked');
-      showPasswordLabel.setAttribute('aria-checked', 'false');
     }
   };
 
-  if(showPassword){
-    showPassword.setStyle('display', 'block');
-  }
+  Y.one('#unmask-password-container').setStyle('display', 'block');
 
-  Y.on('mousedown', togglePasswordMask, '#show-password');
+  Y.on('change', togglePasswordMask, '#unmask-password');
 });
